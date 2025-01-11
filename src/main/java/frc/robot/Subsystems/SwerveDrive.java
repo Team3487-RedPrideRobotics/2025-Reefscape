e7663a020fabf0c96b8e3220cae31779029eb648
3487 Red Pride Robotics <team3487@gmail.com>
2025-01-11 14:43:59 -0500

    Trying to get Yagsl
    Please help 😭

--- src/main/java/frc/robot/Subsystems/SwerveDrive.java
+++ src/main/java/frc/robot/Subsystems/SwerveDrive.java
@@ -1,13 +1,17 @@
-package main.java.frc.robot.Subsystems;
+package frc.robot.Subsystems;
 
-import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.util.Units;
-import edu.wpi.first.math.kinematics.SwerveModuleState;
-import edu.wpi.first.math.kinematics.SwerveModulePosition;
-import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import swervelib.imu.NavXSwerve;
+import swervelib.SwerveModule;
 
 
 
