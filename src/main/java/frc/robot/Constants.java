f0b9014b66856e09f09a987bf45c004eed75e773
Illusionist2732 <happenose@hotmail.com>
2025-02-27 16:00:35 -0800

    test

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -27,7 +27,7 @@ public final class Constants {
     public static class OperatorConstants
     {
 
-        // Joystick Deadband
+        // Joystick Deadband, and other things
         public static final double LEFT_X_DEADBAND  = 0.5;
         public static final double LEFT_Y_DEADBAND  = 0.5;
         public static final double RIGHT_X_DEADBAND = 0.5;
