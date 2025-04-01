9b92428e13b668821c7b93cb5f1bdffe7641b157
3487 Red Pride Robotics <team3487@gmail.com>
2025-04-01 14:42:58 -0400

    Added comments to robot container

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -43,6 +43,17 @@ import frc.robot.Subsystems.Elevator.States.ElevatorManualState;
 import frc.robot.Subsystems.Pivot.ArmSubsystem;
 import frc.robot.Subsystems.Pivot.States.PivotAnalogManual;
 import frc.robot.Subsystems.Pivot.States.PivotManualState;
+<<<<<<< Updated upstream
+=======
+import frc.robot.Subsystems.Pivot.States.PivotZeroState;
+import frc.robot.Subsystems.Swerve.SwerveSubsystem;
+import frc.robot.Subsystems.Swerve.States.ForwardState;
+import frc.robot.Subsystems.Swerve.States.LeftState;
+import frc.robot.Subsystems.Swerve.States.LimelightForwardState;
+import frc.robot.Subsystems.Swerve.States.LimelightLeftState;
+import frc.robot.Subsystems.Swerve.States.LimelightRightState;
+import frc.robot.Subsystems.Swerve.States.RightState;
+>>>>>>> Stashed changes
 import frc.robot.Subsystems.FloorIntake.FloorIntakeSubsystem;
 import frc.robot.Subsystems.FloorIntake.States.IntakePivotState;
 import frc.robot.Subsystems.FloorIntake.States.IntakeState;
@@ -71,6 +82,7 @@ public class RobotContainer {
     pivot = new ArmSubsystem();
     floorIntake = new FloorIntakeSubsystem();
     elevator = new ElevatorSubsystem();
+    //I dont think camera is needed with pi stuff, but scared to remove
     camera = new Camera();
    
     configureDriverBindings();
@@ -82,79 +94,227 @@ public class RobotContainer {
 
 
   private void configureDriverBindings() {
+<<<<<<< Updated upstream
     
     Command driveFieldOrientedAnglularVelocity = drivebase.driveCommand(
+=======
+    //
+    // Creating Double Supplier for elevator sensor, allowing slowing down while elevator high
+    //
+    DoubleSupplier elevatorValue = (() -> elevator.getPosition());
+
+    //
+    // Driving field oriented
+    //
+    Command FieldOriented = drivebase.driveCommand(
+>>>>>>> Stashed changes
       () -> MathUtil.applyDeadband(driverXbox.getLeftY(), DriverConstants.LEFT_Y_DEADBAND),
       () -> MathUtil.applyDeadband(driverXbox.getLeftX(), DriverConstants.LEFT_X_DEADBAND),
       () -> MathUtil.applyDeadband(driverXbox.getRightX(), DriverConstants.RIGHT_X_DEADBAND),
      true);
 
-    Command AHHHH_WHY_HAVE_YOU_FORSAKEN_ME = drivebase.driveCommand( 
+    //
+    // Driving front of robot centric
+    //
+    Command FrontCentric = drivebase.driveCommand( 
       () -> MathUtil.applyDeadband(driverXbox.getLeftY(), DriverConstants.LEFT_Y_DEADBAND),
       () -> MathUtil.applyDeadband(driverXbox.getLeftX(), DriverConstants.LEFT_X_DEADBAND),
       () -> MathUtil.applyDeadband(driverXbox.getRightX(), DriverConstants.RIGHT_X_DEADBAND),
           false);
 
-    Command floorMan = drivebase.driveCommand(
+    //
+    // Driving back of robot centric
+    //
+    Command BackCentric = drivebase.driveCommand(
       () -> MathUtil.applyDeadband(-driverXbox.getLeftY(), DriverConstants.LEFT_Y_DEADBAND),
       () -> MathUtil.applyDeadband(-driverXbox.getLeftX(), DriverConstants.LEFT_X_DEADBAND),
       () -> MathUtil.applyDeadband(driverXbox.getRightX(), DriverConstants.RIGHT_X_DEADBAND),
           false);
 
-    drivebase.setDefaultCommand(driveFieldOrientedAnglularVelocity);
+    drivebase.setDefaultCommand(FieldOriented);
     drivebase.getSubsystem();
     
+    //
+    // A Button: Reset Gyro
+    // X Button: Lock/X wheels
+    // Y Button: Toggle FloorIntake Up
+    // 
     driverXbox.a().onTrue((Commands.runOnce(drivebase::zeroGyro)));
     driverXbox.x().whileTrue(Commands.runOnce(drivebase::lock, drivebase).repeatedly());
+    driverXbox.y().toggleOnTrue(new FloorIntakeZeroState(floorIntake));
 
+<<<<<<< Updated upstream
     driverXbox.leftBumper().whileTrue(AHHHH_WHY_HAVE_YOU_FORSAKEN_ME);
     driverXbox.rightBumper().whileTrue(floorMan);
+=======
+    
+    //
+    // Left Bumper: Activate front centric
+    //
+    driverXbox.leftBumper().whileTrue(FrontCentric);
+
+    //
+    // Right Bumbper: Activate back centric  
+    //
+    driverXbox.rightBumper().whileTrue(BackCentric);
+
+    //
+    // Up Arrow: Limelight Allign "Forward"
+    // Left Arrow: Limelight Allign Left
+    // Right Arrow: Limelight Allign Right
+    //
+    driverXbox.povUp().whileTrue(new LimelightForwardState(drivebase));
+    driverXbox.povLeft().whileTrue(new LimelightLeftState(drivebase));
+    driverXbox.povRight().whileTrue(new LimelightRightState(drivebase));
+
+    //
+    // Left Trigger: Left
+    // Right Trigger: Right
+    //
+    driverXbox.leftTrigger().whileTrue(new LeftState(drivebase));
+    driverXbox.rightTrigger().whileTrue(new RightState(drivebase));
+
+
+>>>>>>> Stashed changes
   }
 
   public void configureOperatorBindings()
   {
 
+    //
+    // Right Joystick Up and Down, effects the floorIntake's pivot
+    //
     floorIntake.setDefaultCommand(
       new IntakePivotState(
         floorIntake, 
         () -> MathUtil.applyDeadband(operatorXbox.getRightY(), OperatorConstants.RIGHT_Y_DEADBAND))
       );
 
+    //
+    // Left Trigger turns the pivot "out"
+    //
     pivot.setDefaultCommand(
       new PivotManualState(
         pivot, 
         () -> MathUtil.applyDeadband(operatorXbox.getLeftTriggerAxis(), OperatorConstants.LEFT_TRIGGER_DEADBAND))
       );
 
+    //
+    // Left Bumper turns the pivot "in"
+    //
     operatorXbox.leftBumper().whileTrue(new PivotAnalogManual(pivot));
 
+    //
+    // Left Joystick Up and Down, effects the elevator
+    //
     elevator.setDefaultCommand(
       new ElevatorManualState(
         elevator, 
         () -> MathUtil.applyDeadband(operatorXbox.getLeftY(), OperatorConstants.LEFT_Y_DEADBAND))
     );
 
+<<<<<<< Updated upstream
     operatorXbox.rightBumper().whileTrue(new IntakeState(floorIntake, 1));
     operatorXbox.rightTrigger(0.2).whileTrue(new OuttakeState(floorIntake, 1));
+=======
+    //
+    // Right Bumper intakes both pivot and floor intake
+    //
+    operatorXbox.rightBumper().whileTrue(new ParallelCommandGroup(
+      new IntakeState(floorIntake, 1),
+      new ArmSuckState(pivot)
+    ));
+
+    //
+    // Right Trigger outtakes both pivot and floor intake
+    //
+    operatorXbox.rightTrigger(0.2).whileTrue( 
+      new ParallelCommandGroup(new ParallelCommandGroup(
+        new OuttakeState(floorIntake, 1),
+        new ArmShootState(pivot)
+        )));
+
+    //
+    // Y button: Elevator L4
+    // B button: Elevator L3
+    // X button: Elevator L2
+    // A Button: Elevator Home
+    //
+    operatorXbox.y().whileTrue(new ElevatorPermL4State(elevator));
+    operatorXbox.b().whileTrue(new ElevatorPermL3State(elevator));
+    operatorXbox.x().whileTrue(new ElevatorPermL2State(elevator));
+    operatorXbox.a().whileTrue(new ElevatorBaseState(elevator));
+    
+    //
+    // Up arrow moves pivot to an angle to score L4
+    //
+    operatorXbox.povUp().whileTrue(new PivotL4ScoredState(pivot));
+
+    //
+    // Down arrow moves elevator to intake position, moves pivot to intake angle, and intakes pivot
+    //
+    operatorXbox.povDown().whileTrue(
+      new ParallelCommandGroup(
+          new PivotIntakeState(pivot), 
+          new ElevatorIntakeState(elevator)));
+>>>>>>> Stashed changes
 
 
   }
  
   public Command getAutonomousCommand() {
+<<<<<<< Updated upstream
     //return autoChooser.getSelected();
     return new PathPlannerAuto("A-P4_T10;");
   }
 
   public void buildNamedCommands(){
     NamedCommands.registerCommand("Floor Intake Shoot", new SlowOuttakeState(floorIntake));
+=======
+    // returns what the AutoChooser selected
+    return autoChooser.getSelected();
+  }
+
+  public void buildNamedCommands(){
+
+    //
+    // Floor Intake Angle
+    //
+    NamedCommands.registerCommand("Floor Pivot Trough", new TroughState(floorIntake));
+    NamedCommands.registerCommand("Floor Pivot Home", new FloorIntakeZeroState(floorIntake));
+    NamedCommands.registerCommand("Floor Intake Shoot", new OuttakeState(floorIntake, 1).withTimeout(2));
+
+    //
+    // Elevator
+    //
+    NamedCommands.registerCommand("Elevator L4", new ElevatorL4State(elevator));
+
+    //
+    // Pivot
+    //
+    NamedCommands.registerCommand("Pivot L4", new PivotL4ScoredState(pivot));
+    NamedCommands.registerCommand("Pivot Zero", new PivotZeroState(pivot));
+    NamedCommands.registerCommand("Arm Shoot", new ArmShootState(pivot).withTimeout(1));
+    NamedCommands.registerCommand("Claw Intake",   new ParallelCommandGroup(new PivotIntakeState(pivot), new ElevatorIntakeState(elevator)));
+
+    //
+    // Driving
+    //
+    NamedCommands.registerCommand("Limelight Left", new LimelightLeftState(drivebase));
+    NamedCommands.registerCommand("Limelight Forward", new LimelightForwardState(drivebase));
+    NamedCommands.registerCommand("Forward 0.5", new ForwardState(drivebase).withTimeout(0.5));
+    NamedCommands.registerCommand("Forward 1.5", new ForwardState(drivebase).withTimeout(1.5));
+>>>>>>> Stashed changes
   }
 
   public void setMotorBrake(boolean brake)
   {
+    //Sets drive motors to brake mode when given true value
     drivebase.setMotorBrake(brake);
   }
   
   public static void BuildAutoChooser(){
+    //Creates the auto chooser, and then adds it to shuffleboard
     autoChooser = AutoBuilder.buildAutoChooser();
     SmartDashboard.putData("Auto Chooser", autoChooser);
   }
